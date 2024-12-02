39886e8af0fffcc689cd01149114c9c39bbc3723
OliverAnderson <oliverandersonllorens18@gmail.com>
2024-12-02 18:40:44 +0100

    POST, GET, PUT y DELETE hecho

--- backend.py
+++ backend.py
@@ -6,8 +6,8 @@ import uuid
 app = Flask(__name__)
 
 
-@app.route("/send", method=["POST"])
-def send_message(messages):
+@app.route("/send", methods=["POST"])
+def send_message():
     data = request.json
     if not data or "content" not in data:
         return jsonify({"Error": "Content is required"}), 400
@@ -17,19 +17,34 @@ def send_message(messages):
     return jsonify({uid: data["content"]}), 200
 
 
-@app.route("/receive", method=["GET"])
-def receive_message():
-    return 0
+@app.route("/receive/<message_id>", methods=["GET"])
+def receive_message(message_id):
+    if message_id not in messages:
+        return jsonify({"Error": "Message not found"}), 404
+
+    return jsonify({message_id: messages[message_id]}), 200
 
 
-@app.route("/modify", method=["PUT"])
-def modify_resource():
-    return 0
+@app.route("/modify/<message_id>", methods=["PUT"])
+def modify_resource(message_id):
+    if message_id not in messages:
+        return jsonify({"Error": "Message not found"}), 404
+
+    data = request.json
+    if not data or "content" not in data:
+        return jsonify({"Error": "Content is required"}), 400
+
+    messages[message_id] = data["content"]
+    return jsonify({message_id: messages[message_id]}), 200
 
 
-@app.route("/delete", method=["DELETE"])
-def delete_resource():
-    return 0
+@app.route("/delete", methods=["DELETE"])
+def delete_resource(message_id):
+    if message_id not in messages:
+        return jsonify({"Error": "Message not found"}), 404
+
+    del messages[message_id]
+    return jsonify({"Message": "Deleted successfully"}), 200
 
 
 if __name__ == "__main__":
