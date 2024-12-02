10b804aff41d9d4feeaa3f0b3bfe6c637ebb2003
OliverAnderson <oliverandersonllorens18@gmail.com>
2024-12-02 18:24:05 +0100

    POST hecho

--- backend.py
+++ backend.py
@@ -1,12 +1,20 @@
 from flask import Flask
-from flask import jsonify
+from flask import request, jsonify
+from db import messages
+import uuid
 
 app = Flask(__name__)
 
 
 @app.route("/send", method=["POST"])
-def send_message():
-    return 0
+def send_message(messages):
+    data = request.json
+    if not data or "content" not in data:
+        return jsonify({"Error": "Content is required"}), 400
+
+    uid = uuid.uuid4().hex
+    messages[uid] = data["content"]
+    return jsonify({uid: data["content"]}), 200
 
 
 @app.route("/receive", method=["GET"])
